c53907509deb04a9494cb2e36c096133dbdff660
Yann Lamtalaa <yann.lamtalaa-consultant@mtahq.org>
2026-07-14 09:11:13 -0400

    Initial Commit

---
+++ CICDPipelinePOC/CICDPipelinePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDPipelinePOCApp.swift
+//  CICDPipelinePOC
+//
+//  Created by Yann Lamtalaa on 7/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CICDPipelinePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDPipelinePOC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CICDPipelinePOC
+//
+//  Created by Yann Lamtalaa on 7/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

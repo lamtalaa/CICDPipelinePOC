7abd0dfe11f73a68576e468842296b49cd57f28e
Yann Lamtalaa <yann.lamtalaa-consultant@mtahq.org>
2026-07-14 09:44:37 -0400

    Create iOS CI/CD pipeline POC

--- CICDPipelinePOCTests/CICDPipelinePOCTests.swift
+++ CICDPipelinePOCTests/CICDPipelinePOCTests.swift
@@ -9,30 +9,30 @@ import XCTest
 @testable import CICDPipelinePOC
 
 final class CICDPipelinePOCTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testPipelineContainsEveryMajorStage() {
+        XCTAssertEqual(PipelineStage.pipeline.count, 6)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testPipelineStartsWithSourceControl() {
+        XCTAssertEqual(
+            PipelineStage.pipeline.first?.title,
+            "Source Control"
+        )
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    func testPipelineEndsWithTestFlight() {
+        XCTAssertEqual(
+            PipelineStage.pipeline.last?.title,
+            "TestFlight"
+        )
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    func testPipelineStageIdentifiersAreUnique() {
+        let identifiers = PipelineStage.pipeline.map(\.id)
 
+        XCTAssertEqual(
+            identifiers.count,
+            Set(identifiers).count
+        )
+    }
 }
